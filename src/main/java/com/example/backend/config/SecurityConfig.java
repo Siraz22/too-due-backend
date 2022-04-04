3fb64d7c2f03582245cb58c9b507cdb002275a25
siraz <siraznaorem07@gmail.com>
2022-04-04 11:41:36 +0530

    testing env variables

--- src/main/java/com/example/backend/config/SecurityConfig.java
+++ src/main/java/com/example/backend/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.example.backend.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -14,11 +18,14 @@ import java.util.ArrayList;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        //super.configure(auth);
-//        auth.inMemoryAuthentication().withUser("siraz").password("password123").authorities("ADMIN");
-//    }
+    @Autowired
+    private Environment env;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //super.configure(auth);
+        auth.inMemoryAuthentication().withUser(env.getProperty("customUsername")).password(env.getProperty("customPassword")).roles("");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,9 +38,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/react-api/genericTask/getTasks", "/react-api/interviewbitTask/getTasks").permitAll()
                 .anyRequest()
                 .authenticated()
-                .and().httpBasic();
+                .and().formLogin();
     }
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 
 }
