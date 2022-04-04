44a5565316bac447f623e639cf342d4cae392caf
siraz <siraznaorem07@gmail.com>
2022-04-04 09:14:37 +0530

    testing env variables

--- src/main/java/com/example/backend/config/SecurityConfig.java
+++ src/main/java/com/example/backend/config/SecurityConfig.java
@@ -2,21 +2,28 @@ package com.example.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import java.util.ArrayList;
+
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        //super.configure(auth);
+//        auth.inMemoryAuthentication().withUser("siraz").password("password123").authorities("ADMIN");
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //        super.configure(http);
-
         //web.ignoring().antMatchers("/react-api/genericTask/getTasks", "react-api/interviewbitTask/getTasks");
-
         http.
                 csrf().disable()
                 .authorizeRequests()
--- src/main/java/com/example/backend/controller/BasicAuthenticationController.java
+++ src/main/java/com/example/backend/controller/BasicAuthenticationController.java
@@ -1,5 +1,7 @@
 package com.example.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BasicAuthenticationController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping(path="/auth-api")
     public String getAuthenticationStatus(){
-        return "You are authenticated!";
+        String info = env.getProperty("customUsername");
+        return info;
     }
 
 }
